85f4b527a380d33b5825b4d2b681e05aafa27e88
naari3 <naari.named@gmail.com>
2018-09-01 17:34:19 +0900

    Surrounded try catch

--- src/bot/emoji.js
+++ src/bot/emoji.js
@@ -7,23 +7,28 @@ const axios = require("axios");
 
 export default async robot => {
   robot.respond(/create ([^ ]+) from ([^ ]+)/, async res => {
-    const name = res.match[1];
-    const image_url = res.match[2];
+    try {
+      const name = res.match[1];
+      const image_url = res.match[2];
 
-    const response = await axios.get(image_url, {
-      responseType: "arraybuffer"
-    });
-    const image = Buffer.from(response.data);
-    const emojiUploader = new SlackEmojiUploader(
-      process.env.SLACK_SUBDOMAIN,
-      process.env.SLACK_XOXS_TOKEN
-    );
+      const response = await axios.get(image_url, {
+        responseType: "arraybuffer"
+      });
+      const image = Buffer.from(response.data);
+      const emojiUploader = new SlackEmojiUploader(
+        process.env.SLACK_SUBDOMAIN,
+        process.env.SLACK_XOXS_TOKEN
+      );
 
-    const result = await emojiUploader.upload(name, image);
-    if (result.ok) {
-      res.send(`tried :${name}:`);
-    } else {
-      res.send(`failed`);
+      const result = await emojiUploader.upload(name, image);
+      if (result.ok) {
+        res.send(`tried :${name}:`);
+      } else {
+        res.send(`failed`);
+      }
+    } catch (error) {
+      res.send("[*ERROR*] " + e.message);
+      robot.logger.error(e);
     }
   });
 };

7b202f5da7b99aa2e889a1a03ee21ac879b2250f
naari3 <naari.named@gmail.com>
2018-09-01 17:24:29 +0900

    Update to dokku

--- dokku_config.sh
+++ dokku_config.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -eu
 
-ssh -t dokku@dokku.hxa.me config:set saikoro-bot \
-      HUBOT_HEROKU_KEEPALIVE_URL=http://saikoro-bot.dokku.hxa.me \
+ssh -t dokku@dokku.hxa.me config:set emoji-bot \
+      HUBOT_HEROKU_KEEPALIVE_URL=http://emoji-bot.dokku.hxa.me \
       NPM_CONFIG_PRODUCTION=false \
       HUBOT_SLACK_TOKEN=$HUBOT_SLACK_TOKEN \
+      SLACK_XOXS_TOKEN=$SLACK_XOXS_TOKEN \
+      SLACK_SUBDOMAIN=$SLACK_SUBDOMAIN \
       TZ="Asia/Tokyo"
